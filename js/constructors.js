acaa2ccb38444c5b71a1386c48c1453c0796d62b
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2019-12-06 14:44:18 +0100

    1

--- js/constructors.js
+++ js/constructors.js
@@ -1,1 +1,7 @@
-let test = 1
+function User(data) {
+  this.userId = data.userId
+  this.username = data.username
+  this.email = data.email
+  this.occupation = data.occupation
+  this.theme = data.theme
+}
