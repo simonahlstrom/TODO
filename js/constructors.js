2c55bef68026500f02692e22883bbfc73432ba24
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2019-12-06 15:31:13 +0100

    working on constructors

--- js/constructors.js
+++ js/constructors.js
@@ -7,15 +7,51 @@ function User(data) {
 }
 
 function Label(data) {
-    this.labelName = data.labelName
-    this.color = data.color
-    this.icon = data.icon
-    this.activated = data.activated
-    this.labelId = data.labelId
-    this.element = $('<div>', {
-        "class": "label",
-    }).css({
-        backgroundImage: "url(" + this.icon + ")",
-        backgroundColor: this.color
-    })
+  this.labelName = data.labelName
+  this.color = data.color
+  this.icon = data.icon
+  this.activated = data.activated
+  this.labelId = data.labelId
+  this.element = $('<div>', {
+      "class": "label",
+  }).css({
+      backgroundImage: "url(" + this.icon + ")",
+      backgroundColor: this.color
+  })
+}
+
+function Task(data, objLabel) {
+  this.taskId = data.taskId
+  this.taskName = data.taskName
+  this.shareCode = data.code
+  this.label = objLabel
+  this.creator = data.creator
+
+  this.done = false
+  this.subtasks = []
+  this.subsDL = function() {
+    //code functionality to get deadline from this.subtasks
+  }
+  this.createTask = function() {
+    // create task w/ icon from labelobject
+    let task = $('div')
+    let icon = this.label.element
+    let name = this.taskName
+    task.append(icon, name)
+
+    //create subtasks
+    for (let subtask of this.subtasks) {
+      let subtaskContainer = $('<div>')
+      let subName = $('<div>')
+      subName.html()
+      
+      if (!this.creator) {
+        let claimBox = $('div', {
+          html = "CLAIM"
+        }).click(function() {
+          // code the function to claim subtask later
+        })
+      }
+    }
+  }
 }
