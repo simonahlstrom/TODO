c8b63195d10c0705a1a46c3211fea6d988a09095
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2019-12-06 14:59:46 +0100

    SQL

---
+++ TODO.sql
@@ -0,0 +1,240 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Dec 02, 2019 at 03:29 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `TODO`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Labels`
+--
+
+CREATE TABLE `Labels` (
+  `labelId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `labelName` varchar(100) NOT NULL,
+  `color` varchar(100) NOT NULL,
+  `icon` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Subtasks`
+--
+
+CREATE TABLE `Subtasks` (
+  `subId` int(11) NOT NULL,
+  `subName` varchar(100) NOT NULL,
+  `deadline` date NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `claimedName` varchar(100) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TaskMembers`
+--
+
+CREATE TABLE `TaskMembers` (
+  `taskId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `creator` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tasks`
+--
+
+CREATE TABLE `Tasks` (
+  `taskId` int(11) NOT NULL,
+  `taskName` varchar(100) NOT NULL,
+  `code` varchar(4) NOT NULL,
+  `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TasksInLabelRel`
+--
+
+CREATE TABLE `TasksInLabelRel` (
+  `taskId` int(11) NOT NULL,
+  `labelId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Theme`
+--
+
+CREATE TABLE `Theme` (
+  `themeId` int(11) NOT NULL,
+  `themeName` varchar(100) NOT NULL,
+  `mainColor` varchar(100) NOT NULL,
+  `subColor` varchar(100) NOT NULL,
+  `accentColor` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `userId` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `occupation` varchar(100) NOT NULL,
+  `themeId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Labels`
+--
+ALTER TABLE `Labels`
+  ADD PRIMARY KEY (`labelId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `Subtasks`
+--
+ALTER TABLE `Subtasks`
+  ADD PRIMARY KEY (`subId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `TaskMembers`
+--
+ALTER TABLE `TaskMembers`
+  ADD KEY `taskId` (`taskId`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `Tasks`
+--
+ALTER TABLE `Tasks`
+  ADD PRIMARY KEY (`taskId`);
+
+--
+-- Indexes for table `TasksInLabelRel`
+--
+ALTER TABLE `TasksInLabelRel`
+  ADD KEY `labelId` (`labelId`),
+  ADD KEY `tasksinlabelrel_ibfk_2` (`taskId`);
+
+--
+-- Indexes for table `Theme`
+--
+ALTER TABLE `Theme`
+  ADD PRIMARY KEY (`themeId`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`userId`),
+  ADD KEY `themeId` (`themeId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Labels`
+--
+ALTER TABLE `Labels`
+  MODIFY `labelId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Subtasks`
+--
+ALTER TABLE `Subtasks`
+  MODIFY `subId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Tasks`
+--
+ALTER TABLE `Tasks`
+  MODIFY `taskId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Theme`
+--
+ALTER TABLE `Theme`
+  MODIFY `themeId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Labels`
+--
+ALTER TABLE `Labels`
+  ADD CONSTRAINT `labels_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Subtasks`
+--
+ALTER TABLE `Subtasks`
+  ADD CONSTRAINT `subtasks_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`);
+
+--
+-- Constraints for table `TaskMembers`
+--
+ALTER TABLE `TaskMembers`
+  ADD CONSTRAINT `taskmembers_ibfk_1` FOREIGN KEY (`taskId`) REFERENCES `Tasks` (`taskId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `taskmembers_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`);
+
+--
+-- Constraints for table `TasksInLabelRel`
+--
+ALTER TABLE `TasksInLabelRel`
+  ADD CONSTRAINT `tasksinlabelrel_ibfk_1` FOREIGN KEY (`labelId`) REFERENCES `Labels` (`labelId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tasksinlabelrel_ibfk_2` FOREIGN KEY (`taskId`) REFERENCES `Tasks` (`taskId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `User`
+--
+ALTER TABLE `User`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`themeId`) REFERENCES `Theme` (`themeId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
