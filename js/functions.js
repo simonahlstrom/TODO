60027417a41cfb38710a6a91ec6eed8516bab2ee
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2019-12-10 11:12:19 +0100

    update

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,6 @@ function gatherData(userId) {
     //get info from
 }
 
-
 //Runs editTask with action to create new task
 $('#add').click(function() {editTask("new")})
 
@@ -14,81 +13,81 @@ function cl(x) {
 function editTask (obj, action) {
 
   //Name of task input
-$('<label>', {for: "taskNameInput", html: "Name of task ", appendTo: "#content"})
-$('<input>', {
-  "id": "taskNameInput",
-  type: "text",
-  appendTo: "#content"
-})
-
-//Add subtask button, input, click function
-$('<div>', {"id": "subtaskInputs", appendTo: "#content"})
-$('<label>', {for: "addSubtask", html: "Add subtask ", appendTo: "#subtaskInputs"})
-$('<input>', {
-  "id": "subtaskNameInput",
-  type: "text",
-  appendTo: "#subtaskInputs"
-})
-$('<input>', {
-  "id": "addSubtask",
-  "class": "round",
-  type: "button",
-  value: "",
-  appendTo: "#subtaskInputs"
-}).click(function() {
-  $('<div>', {html: $('#subtaskNameInput').val(), appendTo: "#subtaskContainer"})
-})
-
-//Subtask list
-$('<div>', {
-  "id": "subtaskContainer",
-  appendTo: "#content"
-}).css({
-  width: "80vw",
-  height: "100px",
-  backgroundColor: "lightgrey"
-})
-
-//Label dropdown
-$('<label>', {for: "labelSelect", html: "Select label ", appendTo: "#content"})
-$('<select>', {
-  "id": "labelSelect",
-  value: "label",
-  appendTo: "#content"
-})
-labels.forEach(function(item) {
-  $('<option>', {
-    value: item,
-    html: item,
-    appendTo: "#labelSelect"
-  })
-})
-
-
-//Edit buttons
-$('<div>', {"class": "flex", "id": "buttonContainer", appendTo: "#content"})
-editTaskButtons.forEach(function(item) {
+  $('<label>', {for: "taskNameInput", html: "Name of task ", appendTo: "#content"})
   $('<input>', {
-    "class": "flex",
-    "id": "editTaskButton" + item,
-    value: item,
-    type: "button",
-    appendTo: "#buttonContainer"
-  }).click(function() {
-    if(item == "Save") {
-      console.log("New task saved")
-      //Save to DB
-    } else if (item == "Delete") {
-      console.log("Task deleted")
-      //Popup, remove from DB
-    } else {
-      //return to Home
-    }
+    "id": "taskNameInput",
+    type: "text",
+    appendTo: "#content"
   })
-})
+
+  //Add subtask button, input, click function
+  $('<div>', {"id": "subtaskInputs", appendTo: "#content"})
+  $('<label>', {for: "addSubtask", html: "Add subtask ", appendTo: "#subtaskInputs"})
+  $('<input>', {
+    "id": "subtaskNameInput",
+    type: "text",
+    appendTo: "#subtaskInputs"
+  })
+  $('<input>', {
+    "id": "addSubtask",
+    "class": "round",
+    type: "button",
+    value: "",
+    appendTo: "#subtaskInputs"
+  }).click(function() {
+    $('<div>', {html: $('#subtaskNameInput').val(), appendTo: "#subtaskContainer"})
+  })
+
+  //Subtask list
+  $('<div>', {
+    "id": "subtaskContainer",
+    appendTo: "#content"
+  }).css({
+    width: "80vw",
+    height: "100px",
+    backgroundColor: "lightgrey"
+  })
+
+  //Label dropdown
+  $('<label>', {for: "labelSelect", html: "Select label ", appendTo: "#content"})
+  $('<select>', {
+    "id": "labelSelect",
+    value: "label",
+    appendTo: "#content"
+  })
+  labels.forEach(function(item) {
+    $('<option>', {
+      value: item,
+      html: item,
+      appendTo: "#labelSelect"
+    })
+  })
 
 
-if (action != "new") {
-  //prefilled inputs from obj
-}
+  //Edit buttons
+  $('<div>', {"class": "flex", "id": "buttonContainer", appendTo: "#content"})
+  editTaskButtons.forEach(function(item) {
+    $('<input>', {
+      "class": "flex",
+      "id": "editTaskButton" + item,
+      value: item,
+      type: "button",
+      appendTo: "#buttonContainer"
+    }).click(function() {
+      if(item == "Save") {
+        console.log("New task saved")
+        //Save to DB
+      } else if (item == "Delete") {
+        console.log("Task deleted")
+        //Popup, remove from DB
+      } else {
+        //return to Home
+      }
+    })
+  })
+
+
+  if (action != "new") {
+    //prefilled inputs from obj
+  }
 }
