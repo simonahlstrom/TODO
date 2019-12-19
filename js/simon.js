1f413bc813c405887922b242f6d033ae5bd10766
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2019-12-19 11:59:28 +0100

    doing menu, added jquery-mobile to scripts-folder

--- js/simon.js
+++ js/simon.js
@@ -1,6 +1,6 @@
 // GENERAL FUNCTIONS
 // info about user from cookies, logged in or not
-let loggedIn = false
+let loggedIn = true
 let timeout = true
 
 // create a popup to be removed with external clickevent or timeout if the timeout parameter is passed
@@ -36,7 +36,7 @@ function hidePopup() {
 // handling login and welcome message
 function init() {
   if (loggedIn) {
-    // home()
+    setup(2)
   } else {
     let info = [
       $("<h2>Welcome!</h2>"),
@@ -151,4 +151,82 @@ function login() {
   popup(loginUI)
 }
 
+$("#home").click(() => {
+  showLabels()
+})
+
+function showLabels() {
+  $(".labelBox").toggleClass("activeLabel")
+  
+  for (let label of allLabels) {
+    $("#labels").append(label.element).click(() => {
+      $(".slideIn").addClass("slideIn-active")
+    })
+  }
+}
+
 init()
+
+// functionality for holding down mouse or finger
+
+// The item (or items) to press and hold on
+// function hold(items) {
+//   let item = items
+   
+//   let timerID;
+//   let counter = 0;
+  
+//   let pressHoldEvent = new CustomEvent("pressHold");
+  
+//   // Increase or decreae value to adjust how long
+//   // one should keep pressing down before the pressHold
+//   // event fires
+//   let pressHoldDuration = 50;
+  
+//   // Listening for the mouse and touch events    
+//   item.addEventListener("mousedown", pressingDown, false);
+//   item.addEventListener("mouseup", notPressingDown, false);
+//   item.addEventListener("mouseleave", notPressingDown, false);
+  
+//   item.addEventListener("touchstart", pressingDown, false);
+//   item.addEventListener("touchend", notPressingDown, false);
+  
+//   // Listening for our custom pressHold event
+//   item.addEventListener("pressHold", doSomething, false);
+  
+//   function pressingDown(e) {
+//     // Start the timer
+//     requestAnimationFrame(timer);
+  
+//     e.preventDefault();
+  
+//     console.log("Pressing!");
+//   }
+  
+//   function notPressingDown(e) {
+//     // Stop the timer
+//     cancelAnimationFrame(timerID);
+//     counter = 0;
+  
+//     console.log("Not pressing!");
+//   }
+  
+//   //
+//   // Runs at 60fps when you are pressing down
+//   //
+//   function timer() {
+//     console.log("Timer tick!");
+  
+//     if (counter < pressHoldDuration) {
+//       timerID = requestAnimationFrame(timer);
+//       counter++;
+//     } else {
+//       console.log("Press threshold reached!");
+//       item.dispatchEvent(pressHoldEvent);
+//     }
+//   }
+  
+//   function doSomething(e) {
+//     console.log("pressHold event fired!");
+//   }
+// }
