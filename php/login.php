711fe3a7c17788f54a94d59f909eb90423950c5a
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2019-12-30 21:45:17 +0100

    isak

--- php/login.php
+++ php/login.php
@@ -2,7 +2,7 @@
 include('connectToDB.php');
 
 $pdo = connectDB();
-$query = "SELECT userId, username, password FROM user WHERE username = ? AND password = ?";
+$query = "SELECT userId, username, password FROM User WHERE username = ? AND password = ?";
 $sql = $pdo->prepare($query);
 $sql->bindParam(1, $_GET['username']);
 $sql->bindParam(2, $_GET['password']);
