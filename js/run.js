00ecfcdc5309c34848d2fd286784f1fccd2efb43
simonahlstrom <54178540+simonahlstrom@users.noreply.github.com>
2020-01-09 14:08:46 +0100

    moved everything from simon.js to functions.js

--- js/run.js
+++ js/run.js
@@ -21,6 +21,18 @@ $("#todoLogo").click(()=>{
     home()
     toggleMenu("task", 0)   
 })
+$("#filter").click((event)=>{  
+    toggleMenu("filter", 100)
+    
+  })
+  
+  $("#archive").click((event)=>{
+    toggleMenu("archive", 100)
+  })
+  // show menu with labels
+$('#home').click(() => {
+    toggleMenu("label", 100)
+  })
 
 // onload functions
 getColorsAndIcons()
